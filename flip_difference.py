bee2505e8e80595cdd5a2bced6135f2c4d1aa350
Max <max@recoset.com>
2014-09-13 14:33:08 -0400

    breaking into two parts

--- flip_difference.py
+++ flip_difference.py
@@ -7,7 +7,6 @@ import numpy as np
 img1 = Image.open("example.png")
 img2 = img1.transpose(Image.FLIP_LEFT_RIGHT)
 diff = ImageChops.difference(img1, img2)
-identicals = 
 
 img2.save("example_flip.png")
 diff.save("example_diff.png")
